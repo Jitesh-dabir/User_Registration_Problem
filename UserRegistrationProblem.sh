22aa7ead1dc56b10a5062682adf46e0de68cb545
Jitesh-Dabir <jitudabir9@gmail.com>
2020-03-04 09:33:53 +0530

    Added: User need to valid first name

--- UserRegistrationProblem.sh
+++ UserRegistrationProblem.sh
@@ -1,3 +1,17 @@
 #!/bin/bash -x
 
 echo ".......................................Welcome to User Registration Problem........................................"
+
+#FUNCTION TO VALID FIRST NAME(FIRST LETTER CAPITAL AND MIN 3 CHARACTER)
+function firstName(){
+	local pattern="^[A-Z]{1}[a-z]{2,}$"
+	read -p "Enter First name:" name
+	if [[ $name =~ $pattern ]]
+	then
+		echo "True"
+	else
+		echo "false"
+	fi 
+}
+
+firstName
