1f972ff00c1ccc9da50a6c24188fbd6e28c67881
Jitesh-Dabir <jitudabir9@gmail.com>
2020-03-04 09:51:33 +0530

    Added: User needs to valid last name

--- UserRegistrationProblem.sh
+++ UserRegistrationProblem.sh
@@ -5,13 +5,26 @@ echo ".......................................Welcome to User Registration Proble
 #FUNCTION TO VALID FIRST NAME(FIRST LETTER CAPITAL AND MIN 3 CHARACTER)
 function firstName(){
 	local pattern="^[A-Z]{1}[a-z]{2,}$"
-	read -p "Enter First name:" name
+	read -p "Enter first name:" name
 	if [[ $name =~ $pattern ]]
 	then
-		echo "True"
+		echo "Valid"
 	else
-		echo "false"
+		echo "Invalid"
 	fi 
 }
 
+#FUNCTION TO VALID FIRST NAME AND LAST NAME(FIRST LETTER CAPITAL AND MIN 3 CHARACTER)
+function lastName(){
+   local pattern="^[A-Z]{1}[a-z]{2,}( )[A-Z]{1}[a-z]{2,}$"
+   read -p "Enter first name and last name:" name
+   if [[ $name =~ $pattern ]]
+   then
+      echo "Valid"
+   else
+      echo "Invalid"
+   fi 
+}
+
 firstName
+lastName
