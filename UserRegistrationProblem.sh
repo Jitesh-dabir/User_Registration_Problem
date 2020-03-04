cbf9ad77eac434005b3b6f9b0de60f59cb9c9086
Jitesh-Dabir <jitudabir9@gmail.com>
2020-03-04 10:32:58 +0530

    Added: User need to valid email adress

--- UserRegistrationProblem.sh
+++ UserRegistrationProblem.sh
@@ -16,15 +16,26 @@ function firstName(){
 
 #FUNCTION TO VALID FIRST NAME AND LAST NAME(FIRST LETTER CAPITAL AND MIN 3 CHARACTER)
 function lastName(){
-   local pattern="^[A-Z]{1}[a-z]{2,}( )[A-Z]{1}[a-z]{2,}$"
-   read -p "Enter first name and last name:" name
-   if [[ $name =~ $pattern ]]
-   then
-      echo "Valid"
-   else
-      echo "Invalid"
-   fi 
+	local pattern="^[A-Z]{1}[a-z]{2,}( )[A-Z]{1}[a-z]{2,}$"
+	read -p "Enter first name and last name:" name
+	if [[ $name =~ $pattern ]]
+	then
+		echo "Valid"
+	else
+		echo "Invalid"
+	fi 
 }
-
+#FUNCTION TO VALID EMAIL ADDRESS
+function validEmail(){
+	local pattern="^[a-z]+[0-9]*+@[a-z]+.[a-z]{0,4}?[a-z]{0,4}.[a-z]{1,4}$"
+	read -p "Enter email address:" email
+	if [[ $email =~ $pattern ]]
+	then
+		echo "Valid"
+	else
+		echo "Invalid"
+	fi
+}
 firstName
 lastName
+validEmail
